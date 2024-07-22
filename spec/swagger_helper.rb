1b5bcd6d4482a5e006682a009387a2952d0ae580
Haidamac <serge.krinitsa@gmail.com>
2024-07-23 00:08:27 +0300

    update swagger config

--- spec/swagger_helper.rb
+++ spec/swagger_helper.rb
@@ -39,7 +39,7 @@ RSpec.configure do |config|
           url: 'http://{defaultHost}',
           variables: {
             defaultHost: {
-              default: '127.0.0.1:3000'
+              default: 'localhost:3000'
             }
           }
         }
