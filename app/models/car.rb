c9b32e49f2acadf269fcf51adb42620218fd6163
Haidamac <serge.krinitsa@gmail.com>
2024-07-23 00:03:55 +0300

    update car model

--- app/models/car.rb
+++ app/models/car.rb
@@ -8,5 +8,6 @@ class Car < ApplicationRecord
   scope :filter_by_participant, ->(user) { where(user_id: user) }
   scope :filter_by_status, ->(status) { where(status:) }
 
-
+  validates :brand, :car_model, :body, :mileage, :color, :fuel, :year, :volume, presence: true
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
 end
